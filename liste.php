6c4cdabf6d4a3ccf8e4910374780549796cdea13
Théo <theo.peresse-gourbil@edu.esiee.fr>
2019-06-11 15:05:47 +0200

    authentification pemier for

--- liste.php
+++ liste.php
@@ -12,15 +12,19 @@
 	$tableau = $_GET;
 	$url = "http://147.215.191.33/pr_1101/ws_rasp.php";
 	$methode = "GET";
-
-	$retour_json = lancerRequeteHTTP($url, $methode, $tableau);
-	$retour = json_decode($retour_json,TRUE);
-	$type_retour = gettype($retour);
-	echo "<form action=\"fichier a faire nous meme\" methode=".$methode." name=\"Session\">";
-	echo "Session : <select name=\"session\">";
-	foreach ($_GET as $key => $value) {
-		echo "<option value=".$value.">".$value."</option>";
+	if ($tableau["user"]=="jekill" & $tableau["pwd"]=="congrat_10"){
+		$retour_json = lancerRequeteHTTP($url, $methode, $tableau);
+		$retour = json_decode($retour_json,TRUE);
+		$type_retour = gettype($retour);
+		echo "<form action=\"fichier a faire nous meme\" methode=".$methode." name=\"Session\">";
+		echo "Session : <select name=\"session\">";
+		foreach ($retour as $key => $value) {
+			echo "<option value=".$value.">".$value."</option>";
+		}
+	} else {
+		echo "L'authentification a echouée";
 	}
+	
 ?>
 
 </body>
